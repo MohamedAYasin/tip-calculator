0810584d3e93ade96c70eaae84d82298c2ed1b2b
Your Name <mayasinalu@gmail.com>
2023-05-28 09:15:27 +0300

    Tip calculator JS

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
-// Write your code here.
+// Building a simple tip calculator using basic JS
+
+const bill = 80;
+let tip;
+
+if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15;
+} else {
+    tip = bill * 0.2;
+}
+
+const total = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
